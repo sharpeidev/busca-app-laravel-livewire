dd33d801f4bb2ac7f5df1485cd56e6628a1f7071
Jonas Magalhães <info.jonas@gmail.com>
2025-03-14 17:07:25 -0300

    add empty livewire component 'busca'

---
+++ laravel-app/app/Livewire/Busca.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Busca extends Component
+{
+    public function render()
+    {
+        return view('livewire.busca');
+    }
+}
